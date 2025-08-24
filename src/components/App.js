575a7d0419085a298c04dba04727ee15417aef27
Bill Huang <bill.huang2001@gmail.com>
2025-08-24 13:14:13 -0500

    groq-llama3.3: groq-llama3.3: Refactored EvoX homepage to use separate components for each section

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,4 @@
 import React from 'react';
-import { NavLink } from 'app/router';
-import Image from 'next/image';
-import { HiStar, HiCode } from 'react-icons/hi';
-
-const EvoXHomepage = () => {
-  return (
-    <div className="flex flex-col min-h-screen">
-      {/* News / Updates */}
-      <div className="bg-blue-500 text-white p-2 text-sm">
-        Released EvoX 1.2.1 - EvoX v1.2.1 release is now available, featuring the new Mujoco Playground and an official tutorial!
-      </div>
-
-      {/* Navigation Bar */}
-      <nav className="bg-white shadow-md py-2">
-        <div className="container mx-auto flex justify-between items-center">
-          <Image src="/logo.png" alt="EvoX Logo" width={30} height={30} />
-          <ul className="flex items-center space-x-4">
-            <li>
-              <NavLink href="#" className="text-gray-600 hover:text-gray-900">
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink href="#" className="text-gray-600 hover:text-gray-900">
-                Docs
-              </NavLink>
-import React from 'react';
 import NavigationBar from './NavigationBar';
 import HeroSection from './HeroSection';
 import KeyFeaturesSection from './KeyFeaturesSection';
@@ -58,13 +31,6 @@ const EvoXHomepage = () => {
 
       {/* Footer */}
       <Footer />
-    </div>
-  );
-};
-
-export default EvoXHomepage;
-
-      </footer>
     </div>
   );
 };
